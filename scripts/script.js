c3bec7516dcd77f0846ae8a47aaa148ce034b56f
smeh09 <shauryameh09@gmail.com>
2021-09-19 12:38:54 +0530

    added viewing of lists and tasks

---
+++ scripts/script.js
@@ -0,0 +1,44 @@
+const tasksListsEl = document.querySelector('#tasks-lists')
+
+let tasksLists = [
+    {
+        "name": "Game dev",
+        "tasks": [
+            "learn godot",
+            "make small games",
+            "make dream game"
+        ]
+    },
+    {
+        "name": "Exams prep",
+        "tasks": [
+            "revise for science exams",
+            "get ready for maths exams",
+            "do maths exam practise questions"
+        ]
+    }
+];
+
+const tasksNameList = [];
+
+tasksLists.forEach((list, index) => {
+    tasksNameList.push(list["name"]);
+    tasksListsEl.innerHTML = tasksNameList.map(tasksListName => `<div class='taskListItem'>${tasksListName}</div>`).join('');
+});
+
+const selectedListsTask = document.querySelector('#selected-lists-task');
+
+const tasksListEl = document.querySelector('#tasks-lists');
+
+tasksListEl.addEventListener('click', e => {
+    if (e.target.className === 'taskListItem')
+    {
+        const taskName = e.target.innerHTML;
+        tasksLists.forEach(taskList => {
+            if (taskList['name'] === taskName)
+            {
+                selectedListsTask.innerHTML = taskList['tasks'].map(tasksListasks => `<div class='tasksListTasks'>${tasksListasks}</div>`).join('');
+            }
+        })
+    }
+})
