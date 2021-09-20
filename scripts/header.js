d399e99a557382d1477f81f12191d193f06d2508
smeh09 <shauryameh09@gmail.com>
2021-09-20 16:45:24 +0530

    top link in landing page

--- scripts/header.js
+++ scripts/header.js
@@ -1,11 +1,14 @@
+const topLink = document.querySelector('#top-link ')
 const header = document.querySelector('.header');
 
 const setHeaderHeight = () => {
     if (window.pageYOffset > 100) {
       header.classList.remove('big');
       header.classList.add('small');
+      topLink.style.opacity = 1;
     } else {
         header.classList.remove('small');
+        topLink.style.opacity = 0;
       header.classList.add('big');
     }
 };
